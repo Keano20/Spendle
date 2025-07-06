7e63414c97364db0396fe8f0daae4fe2dc3beab5
Keano R <98560508+Keano20@users.noreply.github.com>
2025-07-06 12:42:19 +0100

    Remove content from the income model and incomeController class as changes will be made to their functionality.

--- Spendle.API/Models/Income.cs
+++ Spendle.API/Models/Income.cs
@@ -2,9 +2,6 @@ namespace Spendle.API.Models
 {
     public class Income
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Source { get; set; } = string.Empty;
-        public decimal Amount { get; set; }
-        public DateTime Date { get; set; } = DateTime.UtcNow;
+        
     }
 }
