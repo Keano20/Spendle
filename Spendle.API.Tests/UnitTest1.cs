0b0d708473b7328fa406de8f87482927eab7407a
Keano R <98560508+Keano20@users.noreply.github.com>
2026-05-28 14:35:41 +0100

    Added Xunit testing to verify password hashing logic.

---
+++ Spendle.API.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace Spendle.API.Tests;
+
+using Spendle.API.Helpers;
+using Xunit;
+
+public class PasswordHelperTests
+{
+    [Fact]
+    public void VerifyPassword()
+    {
+        string originalPassword = "TestPassword123@";
+        string hashedPassword = PasswordHelper.HashPassword(originalPassword);
+
+        bool isMatch = PasswordHelper.VerifyPassword(originalPassword, hashedPassword);
+        Assert.True(isMatch);
+    }
+}
